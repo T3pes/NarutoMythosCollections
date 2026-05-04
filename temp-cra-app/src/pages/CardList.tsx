f201b68ed379bdfbd8c5849136e312f754057d26
T3pes <tepes91@gmail.com>
2026-05-04 10:31:17 +0200

    cambio struttura

---
+++ temp-cra-app/src/pages/CardList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CardList() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-2">Lista Carte</h2>
+      {/* Lista carte, ricerca, filtri, ecc. */}
+      <div>[CardList in costruzione]</div>
+    </div>
+  );
+}
+
+export default CardList;
+
---
+++ temp-cra-app/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-xl font-bold mb-4">Login</h2>
+      {/* Form login Supabase */}
+      <div>[Login in costruzione]</div>
+    </div>
+  );
+}
+
+export default Login;
+
